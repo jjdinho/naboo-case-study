a30d39e15bd8c754fa5d828ae4b038e7797bd392
Jake Johnson <johnson.s.jake@gmail.com>
2026-09-23 22:52:57 +0200

    Point the GetUser note at the rename recommendation

--- front-end/src/graphql/queries/auth/getUser.ts
+++ front-end/src/graphql/queries/auth/getUser.ts
@@ -1,6 +1,7 @@
 import gql from "graphql-tag";
 
-// Fetches the current user as Me, not the public User type. The name predates Me.
+// Fetches the current user as Me, not the public User type. The name predates
+// Me; renaming it GetMe is a recommendation in docs/suggestions.md (theme 3).
 const GetUser = gql`
   query GetUser {
     getMe {
